9cf9fafaba9df2b5ede2736bfd9b0b975e9e90ee
Błażej Sowa <bsowa123@gmail.com>
2026-08-10 10:11:31 +0200

    Fix lint errors

--- raph_fw/raph_fw/commands/hardware_test.py
+++ raph_fw/raph_fw/commands/hardware_test.py
@@ -18,7 +18,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import argparse
 import sys
 import time
 from pathlib import Path
--- raph_fw/raph_fw/commands/motor_check.py
+++ raph_fw/raph_fw/commands/motor_check.py
@@ -22,10 +22,6 @@ from __future__ import annotations
 
 import sys
 import time
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    import argparse
 
 import rclpy
 from raph_interfaces.msg import ServoCommand, WheelCommand
